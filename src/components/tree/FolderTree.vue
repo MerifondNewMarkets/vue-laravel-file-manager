eea8421431ca2a129f63a10223076afd1b73c61f
Remi Ebeling <remi.ebeling@merifond-new-markets.de>
2024-04-16 14:17:08 +0200

    left menu is still kinda sketchy, but works

--- src/components/tree/FolderTree.vue
+++ src/components/tree/FolderTree.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="fm-tree">
-        <div class="fm-tree-disk sticky-top"><i class="bi bi-hdd"></i> {{ rootItem }}</div>
-        <branch v-bind:parent-id="parentId" />
+        <ul class="list-unstyled fm-tree-branch">
+            <li>
+                <p
+                    v-if="rootItem"
+                    class="root-item"
+                    :class="{ selected: isDirectorySelected(rootItem.path) }"
+                    @click="selectDirectory(rootItem.path)"
+                >
+                        <i class="bi bi-hdd"></i> {{ rootItemName }}
+                </p>
+                <branch v-bind:parent-id="parentId" />
+            </li>
+        </ul>
+        <!-- <div class="fm-tree-disk sticky-top"><i class="bi bi-hdd"></i> {{ rootItemName }}</div> -->
+
     </div>
 </template>
 
@@ -18,23 +31,57 @@ export default {
          * Selected Disk
          * @returns {*}
          */
-        rootItem() {
+        rootItemName() {
             if (this.$store.getters['fm/settings/rootPath']) {
-                return this.$store.getters['fm/settings/rootPath'];
+                return `${this.$store.getters['fm/settings/rootPath']}`.split('/').pop();
             }
             return this.$store.getters['fm/selectedDisk'];
+        },
+        rootItem() {
+            return this.$store.getters['fm/tree/directories'].find((item) => item.path === this.$store.getters['fm/settings/rootPath'])
         },
         parentId() {
             if (this.$store.getters['fm/settings/rootPath']) {
                 return this.$store.getters['fm/tree/directories'].find((item) => item.path === this.$store.getters['fm/settings/rootPath'])?.id;
             }
-            return 0;
+            return this.$store.getters['fm/tree/directories'].find((item) => item.parentId === 0).id;
+        },
+        rootItemCssClass (item) {
+            return this.$store.getters['fm/settings/rootPath'] ? 'fm-tree-disk sticky-top' : 'fm-tree-disk';
+        }
+    },
+    methods: {
+        /**
+         * Check, is this directory selected?
+         * @param path
+         * @returns {boolean}
+         */
+         isDirectorySelected(path) {
+            return this.$store.state.fm.left.selectedDirectory === path;
+        },
+        /**
+         * Select directory
+         * @param path
+         */
+        selectDirectory(path) {
+            // only if this path not selected
+            if (!this.isDirectorySelected(path)) {
+                this.$store.dispatch('fm/left/selectDirectory', { path, history: true });
+            }
         },
     },
 };
 </script>
 
 <style lang="scss">
+.root-item {
+    cursor: pointer;
+    &:hover,
+    &.selected {
+        background-color: #f8f9fa;
+
+    }
+}
 .fm-tree {
     overflow: auto;
     border-right: 1px solid #6c757d;
