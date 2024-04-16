5a99e58cab8e5535280ab754e00bbed881100fba
Remi Ebeling <remi.ebeling@merifond-new-markets.de>
2024-04-16 15:05:55 +0200

    Hacked the Shit out of Breadcrumbs. now it seems to work...

--- src/components/manager/BreadCrumb.vue
+++ src/components/manager/BreadCrumb.vue
@@ -2,11 +2,16 @@
     <div class="fm-breadcrumb">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb" v-bind:class="[manager === activeManager ? 'active-manager' : 'bg-light']">
+                <li class="breadcrumb-item" v-on:click="selectMainDirectory">
+                    <span class="badge bg-secondary">
+                        <i class="bi bi-hdd"></i>
+                    </span>
+                </li>
                 <li
                     class="breadcrumb-item text-truncate"
-                    v-for="(item, index) in breadcrumb"
+                    v-for="(item, index) in computedBreadcrumb"
                     v-bind:key="index"
-                    v-bind:class="[breadcrumb.length === index + 1 ? 'active' : '']"
+                    v-bind:class="[computedBreadcrumb.length === index + 1 ? 'active' : '']"
                     v-on:click="selectDirectory(index)"
                 >
                     <span>{{ item }}</span>
@@ -51,9 +56,16 @@ export default {
          * Breadcrumb
          * @returns {*}
          */
-        breadcrumb() {
-            return this.$store.getters[`fm/${this.manager}/breadcrumb`];
+        computedBreadcrumb() {
+            var breadCrumbString = structuredClone(this.$store.getters[`fm/${this.manager}/breadcrumb`]).join('/');
+            breadCrumbString = breadCrumbString.replace(this.$store.getters[`fm/settings/rootPath`], '');
+            return (breadCrumbString ?? '')
+                .replace('/','') // poor mans ltrim('/') works because replace only matches first occurance
+                .split('/');
         },
+        breadCrumb () {
+            return this.$store.getters[`fm/${this.manager}/breadcrumb`];
+        }
     },
     methods: {
         /**
@@ -61,7 +73,11 @@ export default {
          * @param index
          */
         selectDirectory(index) {
-            const path = this.breadcrumb.slice(0, index + 1).join('/');
+            var breadCrumbLength = this.$store.getters[`fm/${this.manager}/breadcrumb`].length;
+            var displayedBreadCrumbLength = this.computedBreadcrumb.length;
+            var offset = breadCrumbLength - displayedBreadCrumbLength;
+            index = offset + index;
+            const path = this.breadCrumb.slice(0, index + 1).join('/');
 
             // only if this path not selected
             if (path !== this.selectedDirectory) {
@@ -75,7 +91,7 @@ export default {
          */
         selectMainDirectory() {
             if (this.selectedDirectory) {
-                this.$store.dispatch(`fm/${this.manager}/selectDirectory`, { path: this.$store.state.fm.settings.rootPath ?? null, history: true });
+                this.$store.dispatch(`fm/${this.manager}/selectDirectory`, { path: this.$store.state.fm.settings.rootPath, history: true });
             }
         },
     },
